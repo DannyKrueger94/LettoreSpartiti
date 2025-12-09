ac2db201fad716eb52777f362703c5781a230414
DannyKrueger94 <dennywod94@gmail.com>
2025-12-09 20:35:33 +0100

    Aggiunti nuovi spartiti alla libreria

--- js/spartiti-library.js
+++ js/spartiti-library.js
@@ -6,7 +6,12 @@
 const spartitiLibrary = [
     // Esempio: { title: "Wonderwall", file: "spartiti/wonderwall.pdf" },
     // Aggiungi i tuoi spartiti qui sotto:
-    { title: "LadyGaga Shallow", file: "spartiti/LadyGaga Shallow.pdf" }
+    { title: "LadyGaga Shallow", file: "spartiti/LadyGaga Shallow.pdf"},
+    { title: "Stand By Me - Ben E. King", file: "spartiti/Stand By Me - Ben E. King.pdf"},
+    { title: "The Nightmare Before Christmas", file: "spartiti/The Nightmare Before Christmas.pdf"},
+    { title: "All I Want for Christmas Is You", file: "spartiti/All I Want for Christmas Is You.pdf"}
+    
+
     
 ];
 
